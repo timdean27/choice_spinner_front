8cab5ea9f3e6f14a9ea8cbb16ee4deccf707cc12
timdean27 <timdean27@yahoo.com>
2022-09-10 22:23:56 -0400

    commected Django api through axios

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-
+import Header from './components/Header'
+import Choices from './api_calls/ChoicesAPI'
 
 function App() {
   return (
     <div className="App">
-
+      <Header/>
+      <Choices/>
     </div>
   );
 }
