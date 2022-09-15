f3b7dff411ffd5f96e041a4fbdf634479ef0aeb1
timdean27 <timdean27@yahoo.com>
2022-09-15 13:51:24 -0400

    set up single choice view page

--- src/pages/SingleChoice.js
+++ src/pages/SingleChoice.js
@@ -1,9 +1,31 @@
 import React, {useState , useEffect} from 'react'
 import { useParams } from 'react-router-dom'
+import axios from "axios";
+
 const SingleChoice = (props) => {
 
     let { id } = useParams()
-    const [choiceD, choiceID] = useState([])
+    const [choice, setChoice] = useState(null)
+
+    useEffect(() => {grabChoice()},[id])
+
+    const grabChoice = () => {
+        const REACT_APP_DATABASE_URL_DJANGO = process.env.REACT_APP_DATABASE_URL_DJANGO+id;
+        // const loginEndpoint = "gfoods_view_protected/";
+        console.log(REACT_APP_DATABASE_URL_DJANGO)
+        const headers = {
+          headers: {
+            "Content-Type": "application/json",
+            //   'Authorization': `Bearer ${accessToken}`,
+          },
+        };
+    
+        axios.get(REACT_APP_DATABASE_URL_DJANGO, headers).then((res) => {
+          console.log("data insisde DjGfood fetch", res.data);
+          setChoice(res.data);
+        });
+      };
+
 
   return (
     <div>SingleChoice ID={id}</div>
