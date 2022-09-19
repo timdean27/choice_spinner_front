8fab6631cf8eba34ca3c1f40b46d6ec80d8cdbbc
timdean27 <timdean27@yahoo.com>
2022-09-19 18:17:16 -0400

    Fixed so APi is called when choice is created and renderes

--- src/pages/ListChoices.js
+++ src/pages/ListChoices.js
@@ -1,32 +1,33 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import AddChoiceLogic from "./AddChoiceLogic"
-const ListChoices = ({ choices }) => {
-  console.log("pritinging at top of list pager", choices);
+import AddChoiceLogic from "./AddChoiceLogic";
+const ListChoices = ({ choices, mainAPIGet }) => {
+  console.log("priting at top of list page", choices);
   const [addChoice, setaddChoice] = useState(false);
-
 
   const clickedAddChoice = () => {
     setaddChoice(true);
-  }
+  };
 
-////////////////////////////////
-//rendering page
+  ////////////////////////////////
+  //rendering page
   const loadedData = () => {
     return (
       <div>
-      <div>
-      <ul>
-        {choices.map((choice, index) => (
-          <li key={index}>
-          <Link  to={`/choice/view/${choice.id}`}>
-            {choice.body}
-          </Link>
-          </li>
-        ))}
-        </ul>
-      </div>
-      {!addChoice ?<button onClick={clickedAddChoice}>Add Choice</button> :<AddChoiceLogic setaddChoice={setaddChoice}/>}
+        <div>
+          <ul>
+            {choices.map((choice, index) => (
+              <li key={index}>
+                <Link to={`/choice/view/${choice.id}`}>{choice.body}</Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+        {!addChoice ? (
+          <button onClick={clickedAddChoice}>Add Choice</button>
+        ) : (
+          <AddChoiceLogic setaddChoice={setaddChoice} mainAPIGet={mainAPIGet} />
+        )}
       </div>
     );
   };
