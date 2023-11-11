feefb88ca2eedb5b35c6c3808bd8ae2edc858769
timdean27 <timdean27@yahoo.com>
2023-11-11 12:59:27 -0500

    circle works using d3 pie chart to make even slices for options

--- src/components/ChoiceWheel.js
+++ src/components/ChoiceWheel.js
@@ -1,44 +1,67 @@
-import React, { useState, useEffect } from "react";
-import "../styles/ChoiceWheel.css"; // Import the CSS file for styling
+import React, { useEffect, useRef } from "react";
+import * as d3 from "d3";
+import "../styles/ChoiceWheel.css";
 
-const ChoiceSpinner = ({ choices, onChoiceSelected }) => {
-  const [spinning, setSpinning] = useState(false);
-  const [selectedChoice, setSelectedChoice] = useState(null);
-
-  const spinSpinner = () => {
-    setSpinning(true);
-
-    // Simulate spinning for a few seconds
-    setTimeout(() => {
-      const randomIndex = Math.floor(Math.random() * choices.length);
-      setSelectedChoice(choices[randomIndex]);
-      setSpinning(false);
-      onChoiceSelected(choices[randomIndex]);
-    }, 10000); // Spin for 10 seconds
-  };
+const ChoiceSpinner = ({ choices }) => {
+  const pieChartRef = useRef(null);
 
   useEffect(() => {
-    setSelectedChoice(null);
+    createPieChart();
   }, [choices]);
 
+  const createPieChart = () => {
+    // Remove existing pie chart
+    d3.select(pieChartRef.current).selectAll("*").remove();
+
+    const width = 300;
+    const height = 300;
+    const radius = Math.min(width, height) / 2;
+
+    const svg = d3.select(pieChartRef.current).append("svg")
+      .attr("width", width)
+      .attr("height", height)
+      .append("g")
+      .attr("transform", `translate(${width / 2}, ${height / 2})`);
+
+    const data = choices.map((choice) => choice.body.length);
+
+    const colorScale = d3.scaleOrdinal()
+      .domain(choices.map((choice) => choice.body))
+      .range(d3.schemeCategory10);
+
+    const pie = d3.pie();
+
+    const arc = d3.arc()
+      .innerRadius(0)
+      .outerRadius(radius);
+
+    // Append slices
+    svg.selectAll("path")
+      .data(pie(data))
+      .enter()
+      .append("path")
+      .attr("d", arc)
+      .attr("fill", (d, i) => colorScale(choices[i].body))
+      .attr("class", "pie-slice");
+
+    // Append text to each slice
+    svg.selectAll("text")
+      .data(pie(data))
+      .enter()
+      .append("text")
+      .attr("transform", d => `translate(${arc.centroid(d)})`)
+      .attr("dy", "0.35em") // Adjust vertical positioning
+      .attr("text-anchor", "middle")
+      .text((d, i) => choices[i].body);
+  };
+
   return (
-    <div className={`choice-spinner ${spinning ? "spinning" : ""}`}>
-      <div className="spinner-inner">
-        {choices.map((choice, index) => (
-          <div
-            key={choice.id}
-            className={`spinner-choice ${choice === selectedChoice ? "selected" : ""}`}
-            style={{ transform: `rotate(${(360 / choices.length) * index}deg)` }}
-          >
-            {choice.body}
-          </div>
-        ))}
-      </div>
-      <button onClick={spinSpinner} disabled={spinning}>
-        Spin
-      </button>
+    <div>
+      <h1>ChoiceWheel</h1>
+      <div className="choice-spinner" ref={pieChartRef}></div>
     </div>
   );
 };
 
 export default ChoiceSpinner;
+
